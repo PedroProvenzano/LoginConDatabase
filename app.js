15871e75427d3547155c876a02ceb4139ba2c143
Pedro Provenzano <pedroprov0@gmail.com>
2020-12-04 16:01:16 -0300

    Cambio de puerto

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("./connection");
 require('dotenv/config');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const cors = require('cors');
 
 app.use(cors());
